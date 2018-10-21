c06a17ff8674eb07fc35cdcf2467c43c80bf831e
Jan Hovland <jan.hovland@lyse.net>
2018-10-21 19:41:16 +0200

    Initial Commit

---
+++ teachfirebase/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  teachfirebase
+//
+//  Created by Jan  on 21/10/2018.
+//  Copyright © 2018 Jan . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
