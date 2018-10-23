acb3a6e000b38517225bdb4a6a9e7916e15ea555
Jan Hovland <jan.hovland@lyse.net>
2018-10-23 09:28:29 +0200

    Kan nå legge poster inn i Firebase og hente data

--- teachfirebase/ViewController.swift
+++ teachfirebase/ViewController.swift
@@ -28,7 +28,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         
         // Retrieve the posts and listen for changes
         databaseHandle = ref?.child("Posts").observe(.childAdded, with: { (snapshot) in
-            
+        
             // Try to convert the value of the data into a string
             let post = snapshot.value as? String
             if let actualPost = post {
